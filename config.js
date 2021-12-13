2766736120e294b6b0dbd2adf87c7eefe53dd5b2
baobibuon <95005134+baobibuon@users.noreply.github.com>
2021-12-13 16:24:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Minh Anh à!',
+    introTitle: 'Nhật Vy à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,7 +8,7 @@ const CONFIG = {
     title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích tớ <33',
-    btnNo: 'Không, bẻo à :3',
+    btnNo: 'Không, khánh à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
     btnReply: 'Gửi cho tớ <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
